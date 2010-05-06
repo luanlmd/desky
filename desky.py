d5a7a5efa016483879487b4951e7df154fd493be
Luan Almeida <luanlmd@gmail.com>
2010-05-06 03:02:03 -0300

    Simple theme almost working

--- desky.py
+++ desky.py
@@ -2,30 +2,59 @@ import cairo
 import rsvg
 import gtk
 from time import sleep
-from time import strftime
 import xml.dom.minidom
+import sys
 
 def expose(win, event, dom):
-	cr = win.window.cairo_create()
-	cr.set_source_color(win.style.fg[win.state])
+	ctx = win.window.cairo_create()
 
-	e = dom.getElementById("var1")
-	e = dom.getElementsByTagName("tspan")[0].childNodes[0]
-	e.data = strftime("%H:%M:%S")
-	print "renderizou" + e.data
-	open('/tmp/test.svg','w').write(dom.toxml())
+	if win.is_composited() == False:
+		ctx.set_source_rgb(1, 1, 1)
+		print "no alpha mode available"
+	else:
+		ctx.set_source_rgba(1, 1, 1, 0)
+		print "alpha mode enabled"
+
 	
-	svg = rsvg.Handle(file="/tmp/test.svg")
-	svg.render_cairo(cr)
+	for v in config.vars:
+		#print dir(dom)
+		print "element " + v
+		print "value " + config.vars[v]
+		
+		e = dom.getElementById(v)
+		e = e.getElementsByTagName("tspan")[0].childNodes[0]
+		e.data = config.vars[v]
+	
+	print "rendering"
+	
+	open('/tmp/tmp.svg','w').write(dom.toxml())
+	svg = rsvg.Handle(file="/tmp/tmp.svg")
+	svg.render_cairo(ctx)
 	
 	return True
 
 def main():
-
+	theme = sys.argv[1]
+	theme = theme[0].upper() + theme[1:]
+		
+	#win = gtk.Window(gtk.WINDOW_POPUP)
 	win = gtk.Window()
+	win.set_keep_below(True)
 	win.connect("destroy", lambda w: gtk.main_quit())
+	win.set_decorated(False)
 
-	dom = xml.dom.minidom.parseString(open('test.svg').read())
+	dom = xml.dom.minidom.parseString(open('theme%s.svg' % theme).read())
+	for t in dom.getElementsByTagName('text'):
+			t.setIdAttribute('id')
+			
+	svge = dom.getElementsByTagName('svg')[0]
+
+	width = svge.attributes['width'].value
+	height = svge.attributes['height'].value
+	
+	win.resize(int(width),int(height))
+	win.move(config.x, config.y)
+	#win.skip_taskbar_hint(True)
 
 	win.connect("expose-event", expose, dom)
 	win.show_all()
@@ -37,4 +66,8 @@ def main():
 	"""
 
 if __name__ == '__main__':
-    main()
+	theme = sys.argv[1]
+	theme = theme[0].upper() + theme[1:]
+	exec('import theme%s as config' % theme)
+	main()
+	
---
+++ themeExample.py
@@ -0,0 +1,8 @@
+from time import strftime
+import os
+x = 0
+y = 200
+vars = {}
+vars['var1'] = strftime("%H:%M:%S")
+vars['var2'] = 'Static'
+vars['var3'] = os.popen('uname -a').read()
