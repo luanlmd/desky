150207a028ed0f29c56716b8c6f49c2d90291ace
Luan Almeida <luanlmd@gmail.com>
2011-01-28 00:52:11 -0300

    Added theme support and got a new layout to the default one

--- desky.py
+++ desky.py
@@ -6,17 +6,19 @@ import time
 from math import floor
 import cairo
 import gobject
+import imp
 
-import config
+#import config
 import widget
 
 class Desky():
 
-	def __init__(self):
+	def __init__(self, theme):
 
 		self.win = widget.Window()
 
-		self.dom = xml.dom.minidom.parse('%s/theme.svg' % sys.path[0])
+		self.dom = xml.dom.minidom.parse('%s/themes/%s/theme.svg' % (sys.path[0], theme))
+		self.config = imp.load_source('__main__', '%s/themes/%s/script.py' % (sys.path[0], theme))
 		
 		# Make id be the id
 		for t in self.dom.getElementsByTagName('text'):
@@ -29,14 +31,14 @@ class Desky():
 		
 		# Setting window position
 		self.win.resize(int(floor(float(width))),int(floor(float(height))))
-		self.win.move(config.x, config.y)
+		self.win.move(self.config.x, self.config.y)
 
 		# Set the new draw function to the window
 		self.win.draw = self.draw
 		
 		# Tell the window to update and draw
 		self.update()
-		gobject.timeout_add(config.updateInterval,self.update)
+		gobject.timeout_add(self.config.updateInterval,self.update)
 		
 		self.win.show_all()
 		gtk.main()
@@ -49,18 +51,18 @@ class Desky():
 		
 		print "setting values"
 
-		# Look for new values in the config
-		for v in config.vars:
+		# Look for new values in the self.config
+		for v in self.config.vars:
 			e = self.dom.getElementById(v)
 			e = e.getElementsByTagName("tspan")[0].childNodes[0]
 
-			# Change the svg using the config value
-			print "setting: %s = %s" % (v, config.vars[v])
-			e.data = config.vars[v]
+			# Change the svg using the self.config value
+			print "setting: %s = %s" % (v, self.config.vars[v])
+			e.data = self.config.vars[v]
 			
 
-		# Clean up the config var. In the next loop only new values will be reset
-		config.vars = {}
+		# Clean up the self.config var. In the next loop only new values will be reset
+		self.config.vars = {}
 	
 		cr.set_operator(cairo.OPERATOR_OVER) 
 		svg = rsvg.Handle(data=self.dom.toxml())
@@ -68,6 +70,11 @@ class Desky():
 		print "drawning"
 		svg.render_cairo(cr)
 				
-if __name__ == '__main__':
-	desky = Desky()
+if __name__ == '__main__':	
+	if sys.argv.__len__() > 1:
+		theme = sys.argv[1]
+	else:
+		theme = 'default'
+		
+	desky = Desky(theme)
 	
